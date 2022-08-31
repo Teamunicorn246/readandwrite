1d0f3592804e917b605ef53adcd63a6b2d488cad
Teamunicorn246 <ebba.cederlof@elev.ga.ntig.se>
2022-08-31 14:21:37 +0200

    Added WriteToFile function
    Reads from text files

--- readandwrite/src/Main.cpp
+++ readandwrite/src/Main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
 
 namespace Lib {
 
@@ -8,10 +12,23 @@ namespace Lib {
 	{
 		std::cout << t << "\n";
 	}
+
+	void WriteToFile(std::string filePath)
+	{
+		std::ofstream file(filePath);
+		std::string fileContent;
+		std::getline(std::cin, fileContent);
+		file << fileContent;
+		file.close();
+	}
+
+	std::string filePath1 = "minfil.txt"
 }
+
 
 
 int main()
 {
 	Lib::Print(123);
+	Lib::WriteToFile(Lib::filePath1);
 }
